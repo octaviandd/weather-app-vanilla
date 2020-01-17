21e6a00dc55f7eb519a6feb74b4b031eb3549e09
OCTAVIAN <45077244+octaviandd@users.noreply.github.com>
2020-01-17 22:40:56 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 /** @format */
+console.log("hello Amigo Technology");
 var moment = require("moment");
 const location = document.querySelector("#location");
 const celsiusBtn = document.querySelector("#celsius");
@@ -21,68 +22,70 @@ weather.temperature = {
   unit: "celsius"
 };
 
-if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(setPosition);
-} else {
-  alert("Geolocation");
-}
+window.addEventListener("load", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(setPosition);
+  } else {
+    alert("Geolocation");
+  }
 
-function setPosition(position) {
-  let lat = position.coords.latitude;
-  let long = position.coords.longitude;
+  function setPosition(position) {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
 
-  getWeather(lat, long);
-}
+    getWeather(lat, long);
+  }
 
-function getWeather(lat, long) {
-  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`;
+  function getWeather(lat, long) {
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`;
 
-  fetch(api)
-    .then(function(response) {
-      let data = response.json();
-      return data;
-    })
-    .then(function(data) {
-      kelvinTemp = data.main.temp;
-      weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-      weather.description = data.weather[0].description;
-      weather.iconId = data.weather[0].icon;
-      weather.city = data.name;
-      weather.country = data.sys.country;
-      weather.wind = data.wind.speed;
-      weather.pressure = data.main.pressure;
-      weather.humidity = data.main.humidity;
-    })
-    .then(function() {
-      displayWeather();
+    fetch(api)
+      .then(function(response) {
+        let data = response.json();
+        return data;
+      })
+      .then(function(data) {
+        kelvinTemp = data.main.temp;
+        weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+        weather.description = data.weather[0].description;
+        weather.iconId = data.weather[0].icon;
+        weather.city = data.name;
+        weather.country = data.sys.country;
+        weather.wind = data.wind.speed;
+        weather.pressure = data.main.pressure;
+        weather.humidity = data.main.humidity;
+      })
+      .then(function() {
+        displayWeather();
+      });
+  }
+
+  function displayWeather() {
+    icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.iconId}@2x.png"/>`;
+    temperature.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    sky.innerHTML = weather.description;
+    location.innerHTML = `${weather.city}, ${weather.country}`;
+    wind.innerHTML = `${weather.wind} mph`;
+    pressure.innerHTML = `${weather.pressure} hPa`;
+    humidity.innerHTML = `${weather.humidity} %`;
+
+    fahrenheitBtn.addEventListener("click", function(e) {
+      e.preventDefault();
+      temperature.innerHTML =
+        Math.floor((weather.temperature.value * 9) / 5 + 32) + " °F";
     });
-}
 
-function displayWeather() {
-  icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.iconId}@2x.png"/>`;
-  temperature.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-  sky.innerHTML = weather.description;
-  location.innerHTML = `${weather.city}, ${weather.country}`;
-  wind.innerHTML = `${weather.wind} mph`;
-  pressure.innerHTML = `${weather.pressure} hPa`;
-  humidity.innerHTML = `${weather.humidity} %`;
+    celsiusBtn.addEventListener("click", function(e) {
+      e.preventDefault();
+      temperature.innerHTML = Math.floor(kelvinTemp - 273.15) + " °C";
+    });
 
-  fahrenheitBtn.addEventListener("click", function(e) {
-    e.preventDefault();
-    temperature.innerHTML =
-      Math.floor((weather.temperature.value * 9) / 5 + 32) + " °F";
-  });
+    let date = new Date();
 
-  celsius.addEventListener("click", function(e) {
-    e.preventDefault();
-    temperature.innerHTML = Math.floor(kelvinTemp - 273.15) + " °C";
-  });
-
-  let date = new Date();
-
-  theDate.innerHTML = moment().format("MMM Do YYYY");
-  time.innerHTML = `${date.getHours()} : ${date
-    .getMinutes()
-    .toString()
-    .padStart(2, "0")}`;
-}
+    theDate.innerHTML = moment().format("MMM Do YYYY");
+    time.innerHTML = `${date.getHours()} : ${date
+      .getMinutes()
+      .toString()
+      .padStart(2, "0")}`;
+  }
+});
